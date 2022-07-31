4050f81f232fa182be291ef4bd231cd3a6ba49cd
trishantp <99416253+trishantp@users.noreply.github.com>
2022-07-31 12:19:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+
+  camera = document.getElementById("camera");
+
+
+Webcam.attach(  '#camera'  );
+
+
+
+
+ 
+ 
+
+ 
+
